297cda1045b1f17979864f4c96e807a007b56144
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2016-08-05 23:10:52 +0200

    removing 'x-powered-by' header

--- app/domain/ExpressApp.js
+++ app/domain/ExpressApp.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-var Promise = require("bluebird");
-var _ = require("underscore");
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+var Promise = require('bluebird');
+var _ = require('underscore');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
 /**
  * Express application class
@@ -21,9 +21,10 @@ var ExpressApp = function(entryPoint, app, options){
   self.app = app;
   self.options = options || {};
 
-  expressApp.set("port", entryPoint.port);
-  expressApp.set("alias", entryPoint.alias || "");
-  expressApp.set("allowedHosts", app.Util.Express.mapAllowedHosts(entryPoint.allowedHosts));
+  expressApp.set('x-powered-by', false);
+  expressApp.set('port', entryPoint.port);
+  expressApp.set('alias', entryPoint.alias || '');
+  expressApp.set('allowedHosts', app.Util.Express.mapAllowedHosts(entryPoint.allowedHosts));
 
   expressApp.use(bodyParser.json());
   expressApp.use(bodyParser.urlencoded({ extended: true }));
