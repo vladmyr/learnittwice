c3da0ddabb56e41445191dd47d8663e1d5be8776
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2016-08-07 00:18:21 +0200

    Typecast - ObjectId

--- app/helpers/Util/modules/Typecast.js
+++ app/helpers/Util/modules/Typecast.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('underscore');
+const mongoose = require('mongoose');
 
 /**
  * Type casting utility module
@@ -19,6 +20,24 @@ class Typecast {
     return _.isNaN(cast)
       ? 0
       : cast
+  }
+
+  /**
+   * Cast string representation of ObjectId into ObjectId instance
+   * Note: throws and error if id is not valid ObjectId
+   * @param     {String}    id
+   * @returns   {ObjectId}
+   */
+  static objectId(id) {
+    const ObjectId = mongoose.Types.ObjectId;
+
+    if (id instanceof ObjectId) {
+      return id;
+    } else if (!ObjectId.isValid(id)) {
+      throw new Error('typecast error: id is not valid ObjectId')
+    } else {
+      return new ObjectId(id);
+    }
   }
 }
 
